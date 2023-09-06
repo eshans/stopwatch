ef84254913db3d496f3d21cbc080664a44408fbc
Eric Shans <eric.shans@gmail.com>
2023-09-06 11:23:05 -0400

    updates

--- timer/src/App.jsx
+++ timer/src/App.jsx
@@ -4,19 +4,29 @@ import './App.css'
 function App() {
 	const [randomInput, setRandomInput] = useState('')
 	const renders = useRef(0)
+	const inputRef = useRef()
 
 	const handleChange = (e) => {
 		setRandomInput(e.target.value)
+		renders.current++ //value of ref
+	}
+
+	const focusOnInput = () => {
+		inputRef.current.focus()
 	}
 
 	return (
 		<main className="App">
 			<input
+				ref={inputRef}
 				type="text"
 				value={randomInput}
 				placeholder="random input"
 				onChange={handleChange}
 			/>
+			<p> Renders: {renders.current}</p>
+			<br /> <br />
+			<p>{randomInput}</p>
 		</main>
 	)
 }
