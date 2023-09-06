70d09ca5096edfe3cba6722ca8822c2535e69d34
Eric Shans <eric.shans@gmail.com>
2023-09-06 13:14:28 -0400

    update

--- timer/src/App.jsx
+++ timer/src/App.jsx
@@ -3,22 +3,38 @@ import './App.css'
 
 function App() {
 	const [randomInput, setRandomInput] = useState('')
+	const [seconds, setSeconds] = useState('0')
 	const renders = useRef(0)
-	const inputRef = useRef()
+	const timerID = useRef()
 
 	const handleChange = (e) => {
 		setRandomInput(e.target.value)
 		renders.current++ //value of ref
 	}
 
-	const focusOnInput = () => {
-		inputRef.current.focus()
+	const startTimer = () => {
+		timerID.current = setInterval(() => {
+			renders.current++
+			setSeconds((prev) => prev + 1)
+		}, 1000)
+	}
+
+	const stopTimer = () => {
+		clearInterval(timerID.current)
+		timerID.current = 0 //sets timer to zero
+	}
+
+	const resetTimer = () => {
+		stopTimer()
+		if (seconds) {
+			renders.current++
+			setSeconds(0)
+		}
 	}
 
 	return (
 		<main className="App">
 			<input
-				ref={inputRef}
 				type="text"
 				value={randomInput}
 				placeholder="random input"
@@ -26,7 +42,17 @@ function App() {
 			/>
 			<p> Renders: {renders.current}</p>
 			<br /> <br />
-			<p>{randomInput}</p>
+			<section>
+				<button onClick={startTimer}>Start </button>
+			</section>
+			<section>
+				<button onClick={stopTimer}>Stop </button>
+			</section>
+			<section>
+				<button onClick={resetTimer}>Reset </button>
+			</section>
+			<br /> <br />
+			<p>Seconds : {seconds}</p>
 		</main>
 	)
 }
